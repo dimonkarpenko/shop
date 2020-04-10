e5f96b3c7c7a77f90fae2da414a778656a2d6c48
dima <134.2.zto@gmail.com>
2020-04-10 04:09:02 +0300

    com

--- src/app/app/app-routing.module.ts
+++ src/app/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
     ]
   },
   {
-    path: 'admin', loadChildren: ()=>import('./admin/admin.module').then(m=>m.AdminModule)
+    path: 'admin', loadChildren: './admin/admin.module#AdminModule'
   }
 ];
 
